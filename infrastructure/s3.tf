7c7b99e6fcc201234fcbd19f68b4d6eb137e884f
abhishek <abhishek.shukla@xeneta.com>
2021-10-22 12:08:15 +0200

    Try a new resource

---
+++ infrastructure/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "air-reports" {
+  bucket = "xen-pipeline-via-tf-bucket"
+}
--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,18 +1,16 @@
 terraform {
-  required_version = "0.15.0"
+  backend "remote" {
+    organization = "shopzipp"
+    workspaces {
+      name = "xen-infrastructure"
+    }
+  }
+
+  required_version = ">=0.15.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.57.0"
     }
   }
-  backend "s3" {
-    bucket         = "xen-infra-terraform-pipeline-state"
-    key            = "pipeline.tfstate"
-    encrypt        = false
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-pipeline-state-lock"
-    profile        = "terraform_user_infrastructure"
-  }
 }
-
