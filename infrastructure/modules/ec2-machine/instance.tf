af5c5ab91e77998ea9fc02446562c510fd4dafd3
abhishek <abhishek.shukla@xeneta.com>
2022-02-10 12:47:03 +0100

    looping

--- infrastructure/modules/ec2-machine/instance.tf
+++ infrastructure/modules/ec2-machine/instance.tf
@@ -1,4 +1,10 @@
 resource "aws_instance" "this" {
+  # https://www.terraform.io/language/meta-arguments/count
+   count = var.instance_count
+  # consider this a for loop going for i in range(instance_count) { } i can be accessed via count.index
+  # element(var.instance_count)
+  # https://www.terraform.io/language/meta-arguments/for_each
+  #for_each = toset([for i in range(var.instance_count) : "instance-${i}"])
   ami = var.ami_id
   instance_type = var.instance_type
   tags = local.final_tags
--- infrastructure/modules/ec2-machine/outputs.tf
+++ infrastructure/modules/ec2-machine/outputs.tf
@@ -1,5 +1,12 @@
-output "instance_id" {
-  value = aws_instance.this.id
+output "instance_ids" {
+  value = aws_instance.this[*].id # Splash operator
   description = "Instance Id of the ec2 machine"
   # sensitive = false
 }
+
+// for-each each
+//output "instance_arns" {
+//  value = [for key, instance in aws_instance.this: instance.arn]
+//  description = "Instance Id of the ec2 machine"
+//  # sensitive = false
+//}
